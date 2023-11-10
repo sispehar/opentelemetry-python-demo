58d532bd65891a0747409a931800e0476a7a2af7
Simon Špehar <simon.spehar@flintmail.com>
2023-11-10 09:40:56 +0100

    console exporting working

--- auth_service/app.py
+++ auth_service/app.py
@@ -1,11 +1,31 @@
+import time
+
 from flask import Flask, jsonify, request
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(provider)
+
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
 @app.route('/check_auth', methods=['POST'])
 def check_auth():
-    # Dummy authentication - always return True
-    return jsonify({"authenticated": True}), 200
+    with tracer.start_as_current_span("authenticate") as span:
+        # Dummy authentication - always return True
+        return jsonify({"authenticated": True}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
--- order_service/app.py
+++ order_service/app.py
@@ -1,6 +1,25 @@
-from flask import Flask, jsonify, request
+import time
 import requests
+
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(provider)
+
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
@@ -10,25 +29,29 @@ db = client.purchase_orders
 
 @app.route('/purchase', methods=['GET'])
 def purchase():
-    # Call the auth service
-    auth_response = requests.post('http://auth_service:5001/check_auth')
-    if auth_response.json().get('authenticated'):
-        if check_stock():
-            # Write to MongoDB
-            order_data = {"order": "dummy_order_info"}  # Replace with actual order data as needed
-            db.orders.insert_one(order_data)
-            return jsonify({"status": "success", "message": "Order placed."}), 201
-    return jsonify({"status": "failure", "message": "Authentication failed."}), 401
+    with tracer.start_as_current_span("purchase") as span:
+        # Call the auth service
+        auth_response = requests.post('http://auth_service:5001/check_auth')
+        if auth_response.json().get('authenticated'):
+            if check_stock():
+                # Write to MongoDB
+                order_data = {"order": "dummy_order_info"}  # Replace with actual order data as needed
+                db.orders.insert_one(order_data)
+                return jsonify({"status": "success", "message": "Order placed."}), 201
+        return jsonify({"status": "failure", "message": "Authentication failed."}), 401
 
 @app.route('/orders', methods=['GET'])
 def get_orders():
-    # Retrieve all orders from MongoDB
-    orders = list(db.orders.find({}, {'_id': False}))  # Excluding the default MongoDB '_id' field
-    return jsonify(orders), 200
+    with tracer.start_as_current_span("orders") as span:
+        # Retrieve all orders from MongoDB
+        orders = list(db.orders.find({}, {'_id': False}))  # Excluding the default MongoDB '_id' field
+        return jsonify(orders), 200
 
 def check_stock():
-    # Dummy stock check - always return True for simplicity
-    return True
+    with tracer.start_as_current_span("check_stock") as span:
+        time.sleep(3)
+        # Dummy stock check - always return True for simplicity
+        return True
 
 
 if __name__ == '__main__':
